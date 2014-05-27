42f89e63ebcfaec34e4dbf5083662e4140f18236
lnxslackware <lnxslackware@gmail.com>
2014-05-27 14:47:46 +0300

    Deleted all unnecessary empty lines in the project

--- Labyrinth.cs
+++ Labyrinth.cs
@@ -11,9 +11,6 @@ namespace Labyrinth
     {
         static void Main(string[] args)
         {
-
-
-
             positionX = positionY = 3;  // player position
             flag2 = flag3 = true;
             string[,] labyrinth = new string[7, 7];
@@ -21,7 +18,6 @@ namespace Labyrinth
             while (flag3)
             {
                 Console.WriteLine("Welcome to \"Labyrinth\" game. Please try to escape. Use 'top' to view the top \nscoreboard,'restart' to start a new game and 'exit' to quit the game.\n ");
-
                 flag = flag4 = false;
 
                 while (flag == false)
@@ -29,15 +25,17 @@ namespace Labyrinth
                     LabyrinthGenerator(labyrinth, positionX, positionY);
                     SolutionChecker(labyrinth, positionX, positionY);
                 }
+
                 DisplayLabyrinth(labyrinth);
                 Test(labyrinth, flag2, positionX, positionY);
+                
                 while (flag4) //used for adding score only when game is finished naturally and not by the restart command.
                 {
                     Add(scores, currentMoves);
-
                 }
             }
         }
+
         static void Add(List<Table> s, int m)
         {
             if (s.Count != 0)
@@ -45,22 +43,18 @@ namespace Labyrinth
                 s.Sort(delegate(Table s1, Table s2) { return s1.moves.CompareTo(s2.moves); });
             }
 
-
             if (s.Count == 5)
             {
-
                 if (s[4].moves > m)
                 {
-
                     s.Remove(s[4]);
                     Console.WriteLine("Please enter your nickname");
                     string name = Console.ReadLine();
                     s.Add(new Table(m, name));
                     Table_(s);
-
                 }
-
             }
+
             if (s.Count < 5)
             {
                 Console.WriteLine("Please enter your nickname");
@@ -69,9 +63,7 @@ namespace Labyrinth
                 Table_(s);
             }
 
-
             flag4 = false;
-
         }
 
         static void Table_(List<Table> scores)
@@ -85,38 +77,27 @@ namespace Labyrinth
                 Console.WriteLine("Top 5: \n");
                 scores.ForEach(delegate(Table s)
                 {
-
-
-
                     Console.WriteLine(String.Format(i+". {1} ---> {0} moves", s.moves, s.name));
                     i++;   
                 }
                 );
                 Console.WriteLine("\n");
             }
-
         }
 
         static void Test(string[,] labyrinth, bool flag_temp, int x, int y)
         {
-
             currentMoves = 0;
 
             while (flag_temp)
             {
-
-
                 Console.Write("\nEnter your move (L=left, R=right, D=down, U=up): ");
                 string i = string.Empty;
                 i = Console.ReadLine();
 
-
-
                 switch (i)
                 {
                     case "d":
-
-
                         if (labyrinth[x + 1, y] == "-")
                         {
                             labyrinth[x, y] = "-";
@@ -132,21 +113,13 @@ namespace Labyrinth
 
                         if (x == 6)
                         {
-
-
                             Console.WriteLine("\nCongratulations you escaped with {0} moves.\n", currentMoves);
                             flag_temp = false;
-
                             flag4 = true;
                         }
 
                         DisplayLabyrinth(labyrinth);
-
-
-
-
                         break;
-
                     case "D":
                         if (labyrinth[x + 1, y] == "-")
                         {
@@ -169,12 +142,7 @@ namespace Labyrinth
                         }
 
                         DisplayLabyrinth(labyrinth);
-
                         break;
-
-
-
-
                     case "u":
                         if (labyrinth[x - 1, y] == "-")
                         {
@@ -186,7 +154,6 @@ namespace Labyrinth
                         else
                         {
                             Console.WriteLine("\nInvalid move! \n ");
-
                         }
 
                         if (x == 0)
@@ -197,8 +164,6 @@ namespace Labyrinth
                         }
 
                         DisplayLabyrinth(labyrinth);
-
-
                         break;
                     case "U":
                         if (labyrinth[x - 1, y] == "-")
@@ -211,7 +176,6 @@ namespace Labyrinth
                         else
                         {
                             Console.WriteLine("\nInvalid move! \n ");
-
                         }
 
                         if (x == 0)
@@ -222,11 +186,8 @@ namespace Labyrinth
                         }
 
                         DisplayLabyrinth(labyrinth);
-
-
                         break;
                     case "r":
-
                         if (labyrinth[x, y + 1] == "-")
                         {
                             labyrinth[x, y] = "-";
@@ -237,7 +198,6 @@ namespace Labyrinth
                         else
                         {
                             Console.WriteLine("\nInvalid move! \n ");
-
                         }
 
                         if (y == 6)
@@ -248,10 +208,8 @@ namespace Labyrinth
                         }
 
                         DisplayLabyrinth(labyrinth);
-
                         break;
                     case "R":
-
                         if (labyrinth[x, y + 1] == "-")
                         {
                             labyrinth[x, y] = "-";
@@ -262,7 +220,6 @@ namespace Labyrinth
                         else
                         {
                             Console.WriteLine("\nInvalid move! \n ");
-
                         }
 
                         if (y == 6)
@@ -273,10 +230,8 @@ namespace Labyrinth
                         }
 
                         DisplayLabyrinth(labyrinth);
-
                         break;
                     case "l":
-
                         if (labyrinth[x, y - 1] == "-")
                         {
                             labyrinth[x, y] = "-";
@@ -287,7 +242,6 @@ namespace Labyrinth
                         else
                         {
                             Console.WriteLine("\nInvalid move! \n ");
-
                         }
 
                         if (y == 0)
@@ -298,10 +252,8 @@ namespace Labyrinth
                         }
 
                         DisplayLabyrinth(labyrinth);
-
                         break;
                     case "L":
-
                         if (labyrinth[x, y - 1] == "-")
                         {
                             labyrinth[x, y] = "-";
@@ -312,7 +264,6 @@ namespace Labyrinth
                         else
                         {
                             Console.WriteLine("\nInvalid move! \n ");
-
                         }
 
                         if (y == 0)
@@ -323,19 +274,14 @@ namespace Labyrinth
                         }
 
                         DisplayLabyrinth(labyrinth);
-
-
                         break;
                     case "top":
-
                         Table_(scores);
                         Console.WriteLine("\n");
                         DisplayLabyrinth(labyrinth);
-
                         break;
                     case "restart":
                         flag_temp = false;
-
                         break;
                     case "exit":
                         Console.WriteLine("Good bye!");
@@ -344,14 +290,8 @@ namespace Labyrinth
                     default:
                         Console.WriteLine("Invalid command!");
                         break;
-
                 }
-
             }
         }
-
     }
-
 }
-
-
